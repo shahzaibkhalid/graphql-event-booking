0837da1d687aa4adb8fd4dc2c15b56ea85f687cc
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2019-05-05 02:36:22 +0500

    ✨

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+const Event = require('./models/event');
 
 const app = express();
 const port = 3000;
 const endpoint = '/graphql';
-
-const events = [];
+const mongoURL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@event-booking-gjxen.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`;
 
 app.use(bodyParser.json());
 app.use(endpoint, graphqlHttp({
@@ -42,22 +43,39 @@ app.use(endpoint, graphqlHttp({
   `),
   rootValue: {
     events(args) {
-      return events;
+      return Event.find()
+        .then((events) => {
+          return events.map(event => ({...event._doc}));
+        })
+        .catch((error) => {
+          throw new Error(error);
+        })
     },
     createEvent(args) {
       const { title, description, price, date } = args.eventInput;
-      const event = {
-        _id: Math.random().toString(),
+      const event = new Event({
         title,
         description,
-        price,
-        date,
-      }
-      events.push(event);
-      return event;
+        price, 
+        date: new Date(date),
+      })
+      return event.save()
+        .then((result) => {
+          console.log('result', result);
+          return {...result._doc};
+        })
+        .catch((err) => {
+          console.log('error', err);
+          throw new Error(err);
+        });
     },
   },
   graphiql: true,
 }));
 
-app.listen(port, () => console.log(`Server started on http://localhost:${port}`));
+mongoose.connect(mongoURL)
+  .then(() => {
+    app.listen(port, () => console.log(`Server started on http://localhost:${port}`));
+  }).catch((err) => {
+    console.log('woah', err);
+  });
